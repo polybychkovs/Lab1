ed926ae1b292336a9d9b0d1956fc03577d1016a7
BychkovskajaPolina <polina14ght@mail.ru>
2021-11-24 15:02:15 +0300

    created class cheese

---
+++ src/bsu/rfe/java/group6/lab1/Bychkouskaja/var4/Cheese.java
@@ -0,0 +1,11 @@
+package bsu.rfe.java.group6.lab1.Bychkouskaja.var4;
+
+public class Cheese extends Food{
+    public Cheese() {
+        super("Сыр");
+    }
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+
+}
